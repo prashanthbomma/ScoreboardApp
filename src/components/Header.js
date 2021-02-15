60aa30f79b6d6f9eb72eaecd5c1ae424dfbd7ef7
Prashanth Bomma <prashanthbomma95@gmail.com>
2021-02-15 23:48:10 +0530

    Added support for stopwatch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Stats from './Stats';
+import Stopwatch from './Stopwatch';
 
 const Header = (props) => {
     return (
       <header>
         <Stats players = {props.players} />
         <h1>Scoreboard</h1>
+        <Stopwatch />
       </header>
     );
   };
